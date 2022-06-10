24e2bee182863568509107b09dd5429182ebd39b
hi <schaser0326@daum.net>
2022-06-10 09:29:02 +0900

    yyc_test

---
+++ src/main/java/com/example/eazipsample/EazipsampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.eazipsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazipsampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EazipsampleApplication.class, args);
+    }
+
+}
